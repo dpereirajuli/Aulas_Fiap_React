3026d69bb4997abade007763d1595b0ad75a070d
Daniel Pereira <131875304+dpereirajuli@users.noreply.github.com>
2025-08-24 22:02:29 -0300

    first commit

---
+++ aula2/src_app_page.js
@@ -0,0 +1,9 @@
+import Cartao from "@/components/Cartao";
+
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <Cartao />
+    </div>
+  );
+}
